2d44ae056e72ba0390f9a7bc1db28506558064b2
Aidan Kim <aidankim@ad.unc.edu>
2024-06-25 21:20:19 -0400

    Completed functions for basic operations: Implemented JS so that the calculator can perform basic operations.

--- script.js
+++ script.js
@@ -1,27 +1,60 @@
 // Creating the buttons.
+const screen = document.querySelector('.screen');
+const buttons = document.querySelector('.buttons');
 const numPad = document.querySelector('.num-pad');
+
+let firstNum = 0;
+let secondNum = 0;
+let operation = '';
+let currNum = '';
 
 for (let i = 9; i > 0; i--) {
     const btn = document.createElement('button');
     numPad.appendChild(btn);
     btn.classList.toggle('button');
     btn.textContent = `${i}`;
+    addNumEventListener(btn, i);
 }
 
 const clr = document.createElement('button');
 numPad.appendChild(clr);
 clr.classList.toggle('button');
 clr.textContent = `CLR`;
+clr.addEventListener('click', () => {
+    screen.textContent = '';
+    firstNum = 0;
+    secondNum = 0;
+    operation = '';
+    currNum = '';
+});
 
 const zero = document.createElement('button');
 numPad.appendChild(zero);
 zero.classList.toggle('button');
 zero.textContent = `0`;
+addNumEventListener(zero, 0);
 
 const eq = document.createElement('button');
 numPad.appendChild(eq);
 eq.classList.toggle('button');
 eq.textContent = `=`;
+eq.addEventListener('click', () => {
+    secondNum = Number(currNum);
+    const result = op(firstNum, secondNum, operation);
+    screen.textContent = `${result}`;
+    currNum = `${result}`;
+    operation = '';
+});
+
+const addition = document.querySelector('.add');
+const subtraction = document.querySelector('.sub');
+const multiplication = document.querySelector('.mult');
+const division = document.querySelector('.divide');
+
+addOperationEventListener(addition, '+', 'add');
+addOperationEventListener(subtraction, '-', 'subtract');
+addOperationEventListener(multiplication, '*', 'multiply');
+addOperationEventListener(division, '/', 'divide');
 
 // Functions for carrying out operations input through UI.
 function add(a, b) {
@@ -37,25 +70,42 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-    return a / b;
+    if (b === 0) {
+        return 'Divide by 0.';
+    }
+    return (a / b).toFixed(3);
 }
 
 function op(num1, num2, operator) {
     // Function for performing operations.
-    switch (func) {
+    switch (operator) {
         case 'add':
-            add(num1, num2);
+            return add(num1, num2);
             break;
         case 'subtract':
-            sub(num1, num2);
+            return subtract(num1, num2);
             break;
         case 'multiply':
-            mult(num1, num2);
+            return multiply(num1, num2);
             break;
         case 'divide':
-            divide(num1, num2);
+            return divide(num1, num2);
             break;
     }
 }
 
-// Adding functionality to the calculator.
+function addNumEventListener(element, text) {
+    element.addEventListener('click', () => {
+        screen.textContent += `${text}`;
+        currNum += `${text}`;
+    });
+}
+
+function addOperationEventListener(element, text, op_name) {
+    element.addEventListener('click', () => {
+        screen.textContent += text;
+        firstNum = Number(currNum);
+        operation = op_name;
+        currNum = '';
+    });
+}
